1559e6d4136ef6ea5faddbd1280243293e4e3ea5
mrgreenbdb <42651230+mrgreenbdb@users.noreply.github.com>
2018-09-11 23:49:05 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-idhere',
+    google_ad_client: 'ca-pub-7937441964791896',
     enable_page_level_ads: true
   });
   `;
